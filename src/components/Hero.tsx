47de96130b24145e37f24400dc960e37423efe2c
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2025-01-03 19:40:40 +0500

    remove padding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from "react-type-animation";
 const Hero = () => {
   return (
     <section className="text-gray-600 body-font mt-0">
-      <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col-reverse items-center">
+      <div className="container mx-auto flex py-10 md:flex-row flex-col-reverse items-center">
         <div className="flex-grow pr-8 md:w-1/2 lg:px-20 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font mb-4 font-medium text-gray-200 text-2xl">
             Hello, I am a
